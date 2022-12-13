64fd262aeb3301bcdf987c2c7fedfc8ae3bd163f
Vicenzo7 <deeprajm35@gmail.com>
2022-12-13 22:29:40 +0530

    bug fix for serverConfig.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const { PORT } = require("./config/serverConfig");
 
-const setUpAndStartServer = async () => {
+const setupAndStartServer = async () => {
   // create the express object
-
   const app = express();
 
-  app.use(bodyParser.json);
+  app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.listen(PORT, () => {
-    console.log(`Server up at port ${PORT}`);
-
-    console.log(process.env);
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
   });
 };
 
-setUpAndStartServer();
+setupAndStartServer();
